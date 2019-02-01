d2645069df3c54a3dcb450fa27f6220a98f8ed27
dagfas <dougmc134@yahoo.com>
2019-02-01 11:58:59 -0500

    second update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,37 +17,37 @@ public class Main {
         int score = 0; //current score + sum of 2 random numbers
 
 
+        while (score <= 100) {
 
-        while (score < 100) {
-            System.out.println("Roll a dice");
             rand1 = r.nextInt(6) + 1;
-            rand2= r.nextInt(6) + 1;
+            rand2 = r.nextInt(6) + 1;
 
             sum = rand1 + rand2;
-            System.out.println("try again your rolled dices were " + rand1 + " and " + rand2);
             score = score + sum;
+
+            System.out.println("Roll a dice");
             System.out.println("Current score is " + score);
 
-            if ((rand1 == 1) || (rand2 == 1)){
-                System.out.println("Rolled dices were: " + rand1 + " & " + rand2);
-                System.out.println("No score change because 1 is thrown on either die");
+            if ((rand1 == 1) || (rand2 == 1)) {
                 score = score + 0;
+                System.out.println("Your rolls: " + rand1 + " & " + rand2);
+                System.out.println("No score change because 1 is thrown on either die");
+                System.out.println("Current score: " + score );
+                System.out.println("***************************************************************");
 
-            }
 
-            else if ((rand1 == 1) && (rand2 == 1)){
+            } else if ((rand1 == 1) && (rand2 == 1)) {
 
                 score = score + 25;
-                System.out.println("Rolled 1 twice" + score);
+                System.out.println("Rolled 1 twice " + score);
+                System.out.println("Tw0 1's: " + rand1 + " & " + rand2);
             }
 
-            //System.out.println ("score is: " + score + " therefore exir");
+            //System.out.println ("score is: " + score + " therefore exit");
             //System.out.println(score + " is above or equal to 100, therefore the game is over!!");
 
         }
         System.exit(0);
-
-
 
 
     }
